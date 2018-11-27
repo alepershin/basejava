aef7880ad481b79e78633b773bc6784283f8f156
alepershin <41328186+alepershin@users.noreply.github.com>
2018-11-27 20:58:02 +0400

    Add files via upload

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,38 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage,null);
     }
 
     void save(Resume r) {
+        storage[size()]=r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i=0;
+        while (storage[i]!=null && storage[i].uuid!=uuid)i+=1;
+        return storage[i];
     }
 
     void delete(String uuid) {
+        int i=0;
+        while (storage[i]!=null && storage[i].uuid!=uuid)i+=1;
+        for (int j=i;j<size()-1;j+=1) storage[j].uuid=storage[j+1].uuid;
+        storage[size()-1]=null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] st = new Resume[size()];
+        for (int i=0;i<size();i+=1)st[i]=storage[i];
+        return st;
     }
 
     int size() {
-        return 0;
+        int i=0;
+        while (storage[i]!=null)i+=1;
+        return i;
     }
 }
