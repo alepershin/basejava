b67238dd69a00d26ac6d02b3e33733ec9f4a61f4
Алексей Першин <41328186+alepershin@users.noreply.github.com>
2018-11-29 19:03:42 +0400

    Add files via upload

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,21 +6,22 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    int size = size();
+    int size = 0;
 
     void clear() {
         Arrays.fill(storage, null);
+        size = 0;
     }
 
     void save(Resume r) {
         storage[size] = r;
-        size += 1;
+        size++;
     }
 
     Resume get(String uuid) {
         int i = 0;
-        while (storage[i] != null && storage[i].uuid != uuid) {
-            i += 1;
+        while (i<size && storage[i].uuid != uuid) {
+            i++;
         }
         return storage[i];
     }
@@ -28,13 +29,13 @@ public class ArrayStorage {
     void delete(String uuid) {
         int i = 0;
         while (i < size && storage[i].uuid != uuid) {
-            i += 1;
+            i++;
         }
-        for (int j = i; j < size - 1; j += 1) {
+        for (int j = i; j < size - 1; j++) {
             storage[j].uuid = storage[j + 1].uuid;
         }
         storage[size - 1] = null;
-        size -= 1;
+        size--;
     }
 
     /**
@@ -42,17 +43,13 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] st = new Resume[size];
-        for (int i = 0; i < size; i += 1) {
+        for (int i = 0; i < size; i++) {
             st[i] = storage[i];
         }
         return st;
     }
 
     int size() {
-        int i = 0;
-        while (storage[i] != null) {
-            i += 1;
-        }
-        return i;
+        return size;
     }
 }
